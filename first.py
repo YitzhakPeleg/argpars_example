c1f2999971bfa20d2c639b95a7d5ad20bf4c8c3a
zahi peleg <zahip@algotec.co.il>
2020-06-30 19:18:50 +0300

    where to init task
    collected args depend on task initialization before or after calling the second parser

--- first.py
+++ first.py
@@ -1,15 +1,16 @@
 from trains import Task
 from argparse import ArgumentParser
 
+init_task_before_second_parsing = True
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("-a", default="A")
-    args, unknown_arguments = parser.parse_known_args()  # parse only known arguments.
-    print(f"{__file__}: args = {args}")
-    import second
-    second.main(unknown_arguments)  # call the main of command with additional args
+parser = ArgumentParser()
+parser.add_argument("-a", default="A")
+args, unknown_arguments = parser.parse_known_args()  # parse only known arguments.
+print(f"{__file__}: parsed args = {args}")
+import second
+second.main(init_task_before_second_parsing, unknown_arguments)  # call the main of command with additional args
 
 
-if __name__ == '__main__':
-    main()
+print(f"parameters from task {Task.current_task().id}")
+for key, val in Task.current_task().get_parameters_as_dict().items():
+    print(f"{key} = {val}")
--- second.py
+++ second.py
@@ -2,9 +2,13 @@ from trains import Task
 from argparse import ArgumentParser
 
 
-def main(args: list):
+def main(init_task_before_second_parsing: bool, incoming_args: list):
     parser = ArgumentParser(prog="")
     parser.add_argument("-b", default="B")
-    args, unknown_arguments = parser.parse_known_args()  # parse only known arguments.
-    print(f"{__file__}: args = {args}")
-    Task.init("argparsing", "example using defaults")
+    if init_task_before_second_parsing:
+        Task.init("argparsing", "init task before second parsing", reuse_last_task_id=False)
+        args, unknown_arguments = parser.parse_known_args(incoming_args)  # parse only known arguments.
+    else:
+        args, unknown_arguments = parser.parse_known_args(incoming_args)  # parse only known arguments.
+        Task.init("argparsing", "init task after second parsing", reuse_last_task_id=False)
+    print(f"{__file__}: parsed args = {args}")
